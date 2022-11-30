aa8efeb282cd278ba188d6410f5f76ca78236ce5
jasonxiong11 <jxiong2021@gmail.com>
2022-11-30 14:39:41 -0500

    Initial Commit

---
+++ Big Red Events/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Big Red Events
+//
+//  Created by Jason Xiong on 11/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
